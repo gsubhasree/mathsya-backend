f6ebabf5c2301e1d8b85d53f82f2ff7d2112c84e
indreshp135 <indreshp135@gmail.com>
2023-01-10 01:02:56 +0530

    fix: globalfence

--- routes/search.js
+++ routes/search.js
@@ -5,15 +5,28 @@ router.post("/search", async function (req, res) {
   try {
     const { search } = req.body;
     console.log(search);
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${process.env.REACT_APP_MAPS_API_KEY}`);
-    const data = await response.json();
-    res.send(data);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({ message: "Error getting location" });    
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${process.env.REACT_APP_MAPS_API_KEY}`
+    );
+    const position = await response.json();
+    const answer = await fetch(
+      `https://nominatim.openstreetmap.org/search.php?q=${search}&polygon_geojson=1&format=json`
+    );
+    if (answer.status !== 200) {
+      throw new Error("Error getting location");
     }
+    let polygonCords = await answer.json()
+    polygonCords = polygonCords.filter(function (data) {
+      return (
+        data.geojson.type === "MultiPolygon" || data.geojson.type === "Polygon"
+      );
+    });
+    console.log(position, JSON.stringify(polygonCords[0].geojson.type));
+    res.send({ position, polygonCords });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Error getting location" });
+  }
 });
-
-
 
 module.exports = router;
